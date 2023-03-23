3cd6ec11523fb709df1f04feb24213e987d56ac4
markjoeneilsalas <markjoeneil.salas@gmail.com>
2023-03-23 09:39:42 +0800

    modified:   login.html 	new file:   loginandsignup.js 	modified:   recipe.html 	modified:   signup.html

---
+++ loginandsignup.js
@@ -0,0 +1,55 @@
+
+if(localStorage.getItem("loggedInUser") != null){
+	window.location.replace("index.html");
+}
+
+function login(event) {
+	event.preventDefault();
+	let username = document.getElementById("login-username").value;
+	let password = document.getElementById("login-password").value;
+	let users = JSON.parse(localStorage.getItem("users"));
+	if(users == null){
+		displayMessage("error", "No users found, please sign up!");
+		return;
+	}
+	for(let i=0; i<users.length; i++){
+		if(users[i].username == username){
+			if(users[i].password == password){
+				localStorage.setItem("loggedInUser", username);
+				window.location.replace("index.html");
+				return;
+			} else {
+				displayMessage("error", "Incorrect password!");
+				return;
+			}
+		}
+	}
+	displayMessage("error", "Username not found!");
+}
+
+function signup(event) {
+	event.preventDefault();
+	let username = document.getElementById("signup-username").value;
+	let email = document.getElementById("signup-email").value;
+	let password = document.getElementById("signup-password").value;
+	let users = JSON.parse(localStorage.getItem("users"));
+	if(users == null){
+		users = [];
+	}
+	for(let i=0; i<users.length; i++){
+		if(users[i].username == username){
+			displayMessage("error", "Username already exists!");
+			return;
+		}
+	}
+	let user = {username: username, email: email, password: password};
+	users.push(user);
+	localStorage.setItem("users", JSON.stringify(users));
+	displayMessage("success", "User registered successfully!");
+}
+
+function displayMessage(type, message){
+	let messageDiv = document.getElementById("message");
+	messageDiv.innerHTML = message;
+	messageDiv.style.color = (type == "success") ? "green" : "red";
+}
