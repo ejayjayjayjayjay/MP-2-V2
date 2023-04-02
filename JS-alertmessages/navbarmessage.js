5ed2660ede7e2cb18bc310a383010643e957c2f9
markjoeneilsalas <markjoeneil.salas@gmail.com>
2023-04-02 14:18:46 +0800

    added preloader animation in to the homepage, about, product, cart login and signup and userdashboard

--- JS-alertmessages/navbarmessage.js
+++ JS-alertmessages/navbarmessage.js
@@ -1,4 +1,9 @@
 
 function signInFirst() {
     alert("You need to sign in first before you can use this feature.");
-  }
+}
+
+window.addEventListener('load', function() {
+    const preloaderWrapper = document.querySelector('.preloader-wrapper');
+    preloaderWrapper.classList.add('hide');
+});
